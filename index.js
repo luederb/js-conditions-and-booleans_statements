14e3d3fcf735ce906a69e0618f271cd45c035290
Lueder Budde <lueder.budde@gmail.com>
2023-09-27 16:09:40 +0100

    finished questions 1 to 4

--- index.js
+++ index.js
@@ -5,16 +5,41 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if (SUPER_SECRET_PASSWORD === receivedPassword) {
+  console.log(
+    "received password matches our 'SUPER_SECRET_PASSWORD'\nWelcome! You are logged in as Brunhilde1984"
+  );
+} else {
+  console.log(
+    "received password doesn't macht our 'SUPER_SECRET_PASSWORD' \nAccess denied!"
+  );
+}
+
 // Part 2: Even / Odd
 const number = 6;
 
+if (number % 2 === 0) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
+
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 10000000;
 
+if (numberOfHotdogs <= 5) {
+  console.log("2 euro per hotdog");
+} else if (numberOfHotdogs <= 100) {
+  console.log("1.50 euro per hotdog");
+} else if (numberOfHotdogs <= 1000000) {
+  console.log("1 euro per hotdog");
+} else {
+  console.log("0.10 euro per hotdog");
+}
 // Part 4: Daytime
-const currentHour = 12;
+const currentHour = 18;
 
-const statement = "";
+const statement = currentHour < 17 ? "Still need to learn..." : "Partytime";
 
 console.log(statement);
 
